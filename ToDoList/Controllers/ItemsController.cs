3431b4f883adac26acb8ad24e82690d19d67a4ad
James Cho and Stuart Gill <james@>
2018-11-29 08:33:03 -0800

    adds items functionality within categories

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,43 +1,36 @@
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-    public class ItemsController : Controller
+  public class ItemsController : Controller
+  {
+
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-            List<Item> allItems = Item.GetAll();
-            return View(allItems);
-        }
-
-
-        [HttpGet("/items/new")]
-        public ActionResult New()
-        {
-            return View();
-        }
-        [HttpPost("/items")]
-        public ActionResult Create(string description)
-        {
-            Item myItem = new Item(description);
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
-        {
-            Item.ClearAll();
-            return View();
-        }
-        [HttpGet("/items/{id}")]
-        public ActionResult Show(int id)
-        {
-            Item item = Item.Find(id);
-            return View(item);
-        }
+       Category category = Category.Find(categoryId);
+       return View(category);
     }
+
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category category = Category.Find(categoryId);
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+
+  }
 }
